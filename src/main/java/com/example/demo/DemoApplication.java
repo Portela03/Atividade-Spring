0167059b605e71fc7c9d8b54d7b8c2b26f548dcc
Portela03 <joo.portela03@gmail.com>
2024-04-22 20:06:48 -0300

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    private static final Map<String, String> raToNameMap = new HashMap<>();
+
+    static {
+        
+        raToNameMap.put("24304", "Lucas");
+        raToNameMap.put("654321", "Maria");
+        
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/ra")
+    public String getNameRa(@RequestParam(value = "ra") String ra) {
+       
+        if (raToNameMap.containsKey(ra)) {
+            return raToNameMap.get(ra);
+        } else {
+            return "RA não encontrado";
+        }
+    }
+}
